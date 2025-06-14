ce787c1a95e7f3eb4f8de3bb837cccfdae76b288
Alexander Perov <alexander.perov@gmail.com>
2025-06-14 20:38:15 +0300

    update

--- scripts/pack2g.sh
+++ scripts/pack2g.sh
@@ -4,13 +4,13 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../include/zip.sh"
 
 if [[ "$1" == "--help" || "$1" == "-h" ]]; then
-    echo "Использование: $0 <путь_к_папке> <имя_архива.zip>"
+    echo "Использование: pack2g <путь_к_папке> <имя_архива.zip>"
     echo "Архивирует папку в zip-архив с частями по 2ГБ."
     exit 0
 fi
 
 if [[ $# -ne 2 ]]; then
-    echo "Использование: $0 <путь_к_папке> <имя_архива.zip>"
+    echo "Использование: pack2g <путь_к_папке> <имя_архива.zip>"
     exit 1
 fi
 
